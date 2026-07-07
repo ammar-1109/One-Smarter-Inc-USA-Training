33f55af4cdfee8d21ca5d9182607dd87421a6e15
ammar-1109 <ammaraziz1109@gmail.com>
2026-07-07 09:36:57 +0530

    day 1

---
+++ FirstConsoleApp/First.cs
@@ -0,0 +1,10 @@
+using System;
+
+class First{
+    public void Display(){
+        Console.WriteLine("This method is from class first");
+    }
+
+}
+
+
---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program{
+
+    static void Main(String[] args){
+        Console.WriteLine("Name : Mohammad Ammar");
+        Console.WriteLine("Branch : IT");
+        Console.WriteLine("College : SSGMCE Shegaon");
+
+        First f = new First();
+        f.Display();
+    }
+}
+
+
